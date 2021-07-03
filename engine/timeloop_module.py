27696b809296d3c6ff2da56b8dc062f0c7fbfdc4
Paulo Natel <pnatel@live.com>
2021-07-04 07:11:32 +1000

    code compliance adjustments

--- engine/timeloop_module.py
+++ engine/timeloop_module.py
@@ -3,7 +3,9 @@
 # https://medium.com/greedygame-engineering/an-elegant-way-to-run-periodic-tasks-in-python-61b7c477b679
 
 
-import time, logging, os
+import time
+import logging
+import os
 from timeloop import Timeloop
 from datetime import timedelta
 # Running as standalone or part of the application
@@ -12,14 +14,15 @@ if __name__ == '__main__' or __name__ == 'timeloop_module':
     from loggerinitializer import initialize_logger
     import csv_module as fc
     cfg.load_config()
-else: 
+else:
     import engine.app_config as cfg
-    from engine.loggerinitializer import initialize_logger 
+    from engine.loggerinitializer import initialize_logger
     import engine.csv_module as fc
 
 initialize_logger(cfg._logPath)
 
 tl = Timeloop()
+
 
 @tl.job(interval=timedelta(seconds=cfg._jobInterval))
 def copy_job():
@@ -28,6 +31,7 @@ def copy_job():
     fc.copy_job()
     notification()
     # time.sleep(15)
+
 
 @tl.job(interval=timedelta(seconds=(cfg._jobInterval/2)))
 def update_csv():
@@ -38,20 +42,21 @@ def update_csv():
 # @tl.job(interval=timedelta(seconds=5))
 # def sample_job_every_5s():
 #     print ("5s job current time : {}".format(time.ctime()))
-    
+
 # @tl.job(interval=timedelta(seconds=10))
 # def sample_job_every_10s():
 #     print ("10s job current time : {}".format(time.ctime()))
 
+
 def notification():
     command = cfg._command
     # if os.name == "nt":
-    #     command = "dir" 
+    #     command = "dir"
     # else:
     #     command = "ls -l"
     os.system(command)
     logging.info("Sending Notification : {}".format(command))
 
+
 if __name__ == "__main__":
     tl.start(block=True)
-    
